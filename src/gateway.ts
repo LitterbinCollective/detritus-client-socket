e28f01d0ae253bcd8584b40e6c2b7aa0dd45a5d4
Dan <31395415+cakedan@users.noreply.github.com>
2026-08-02 12:20:25 -0600

    Add better types for gateway events

--- src/gateway.ts
+++ src/gateway.ts
@@ -1068,14 +1068,14 @@ export class Socket extends EventSpewer {
   }
 
   on(event: string | symbol, listener: (...args: any[]) => void): this;
-  on(event: 'close', listener: (payload: {code: number, reason: string}) => any): this;
-  on(event: 'killed', listener: () => any): this;
-  on(event: 'open', listener: (target: BaseSocket) => any): this;
-  on(event: 'packet', listener: (packet: GatewayPackets.Packet) => any): this;
-  on(event: 'ready', listener: () => any): this;
-  on(event: 'socket', listener: (socket: BaseSocket) => any): this;
-  on(event: 'state', listener: ({state}: {state: SocketStates}) => any): this;
-  on(event: 'warn', listener: (error: Error) => any): this;
+  on(event: SocketEvents.CLOSE | 'close', listener: (payload: {code: number, reason: string}) => any): this;
+  on(event: SocketEvents.KILLED | 'killed', listener: (payload: {error?: Error}) => any): this;
+  on(event: SocketEvents.OPEN | 'open', listener: (target: BaseSocket) => any): this;
+  on(event: SocketEvents.PACKET | 'packet', listener: (packet: GatewayPackets.Packet) => any): this;
+  on(event: SocketEvents.READY | 'ready', listener: () => any): this;
+  on(event: SocketEvents.SOCKET | 'socket', listener: (socket: BaseSocket) => any): this;
+  on(event: SocketEvents.STATE | 'state', listener: ({state}: {state: SocketStates}) => any): this;
+  on(event: SocketEvents.WARN | 'warn', listener: (error: Error) => any): this;
   on(event: string | symbol, listener: (...args: any[]) => void): this {
     super.on(event, listener);
     return this;
